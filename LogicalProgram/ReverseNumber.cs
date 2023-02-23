613a735e4115910258297dc4608c326984fd06c0
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-23 23:00:35 +0530

    Reverse a number using while Loop

---
+++ LogicalProgram/ReverseNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class ReverseNumber
+    {
+        public static void GetRevers()
+        {
+            int lastDigit;
+            int reverse = 0;
+
+            Console.WriteLine("Enter the Number to be reverse :");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (number > 0)
+            {
+                lastDigit = number % 10;
+                reverse = reverse * 10 + lastDigit;
+                number = number / 10;
+            }
+            Console.WriteLine("The Reverse Number is :" +reverse);
+        }
+    }
+}
