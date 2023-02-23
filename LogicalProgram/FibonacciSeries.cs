4b82bde0af81e0043589698953444ea40226179c
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-23 17:35:30 +0530

    Fibonacci Series For input Number

---
+++ LogicalProgram/FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class FibonacciSeries
+    {
+        public static void GetSeries()
+        {
+            int Prevoiustoprevious = 0;
+            int Previous = 1;
+            int next;
+
+            Console.WriteLine("Please enter the Number to get FibonacciSeries");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci Series of " + input + " is :");
+            for (int i = 0; i < input; i++)
+            {
+                next = Prevoiustoprevious + Previous;
+                Prevoiustoprevious = Previous;
+                Previous = next;
+
+                Console.WriteLine(next);
+            }
+        }
+    }
+}
---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciSeries.GetSeries();
+        }
+    }
+}
