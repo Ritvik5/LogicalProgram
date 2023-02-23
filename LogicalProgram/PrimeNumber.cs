d0426ea264d43143be317ffd4854e514aa6ed505
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-23 18:18:10 +0530

    Checking the number is Prime or not

---
+++ LogicalProgram/PrimeNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class PrimeNumber
+    {
+        public static void PrimeCheck()
+        {
+            Console.WriteLine("Enter The number to Check");
+            int input = Convert.ToInt32(Console.ReadLine());
+            bool ifEnteredCheck = false;
+
+            for (int i = 2; i < input / 2; i++)
+            {
+                if (input % i == 0)
+                {
+                    Console.WriteLine("The Number is not a Prime Number");
+                    ifEnteredCheck = true;
+                    
+                }
+            }
+            if (ifEnteredCheck == false)
+            {
+                Console.WriteLine("The Number is Prime");
+            }
+        }
+    }
+}
